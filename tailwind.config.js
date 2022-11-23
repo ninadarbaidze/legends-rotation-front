ccdc4abe59251e914a47cbcfca4fccd296109411
ninadarbaidze <ninadarbaidze66@gmail.com>
2022-11-23 19:46:06 +0400

    style: add main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        main_bg: "url('/assets/images/main-wallpaper.jpg')",
+      },
+      backgroundSize: {
+        auto: 'auto',
+        cover: 'cover',
+        contain: 'contain',
+        '50%': '50%',
+        16: '4rem',
+      },
+      fontFamily: {
+        teko: ['Teko'],
+      },
+    },
   },
   plugins: [],
-}
+};
